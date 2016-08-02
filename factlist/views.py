4d9f9f79c5253c6b348224d366a2fdd9585410e0
Jon Weiner <jon@infinitellc.com>
2016-08-01 21:35:16 -0700

    Now have home.html page coming up in browser

--- factlist/views.py
+++ factlist/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello, world. You're at /index.")
-#     return render(request, 'home.html')
+#     return HttpResponse("Hello, world. You're at /index.")
+    return render(request, 'home.html')
